604274e03f98cb5ebaa8675324372ff66af6570c
Hidetaro7 <hidetaro7@gmail.com>
2018-11-03 19:21:07 +0900

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,64 @@
-const AudioVolume = require("./modules/AudioVolume");
-const AudioAnalyzer = require("./modules/AudioAnalyzer");
+/* instajson.then(instadata => {
+  console.log(instadata);
+  const instaContainer = document.querySelector(".instagram-container");
+  instadata.data.forEach(item => {
+    if (item.type === "video") {
+      const instaItemContainer = document.createElement("div");
+      instaItemContainer.className = "insta-item";
+      instaItemContainer.innerHTML = `
+				<div class="insta-thumbnail" style="background-image: url(${
+          item.images.standard_resolution.url
+        })">
+					<div class="insta-title"></div>
+				</div>
+			`;
+      instaContainer.appendChild(instaItemContainer);
+    }
+  });
+}); */
 
-AudioVolume.init();
+Vue.component("insta-card", {
+  props: ["myData"],
+  data: function() {
+    return {};
+  },
+  template: `
+<div class="insta-card">
+<div class="insta-thumbnail" v-bind:style="{ 'background-image': 'url(' + myData.id + ')' }">
+		<div class="insta-title">{{myData.id}}</div>
+	</div>
+	
+</div>
+`,
+  mounted() {
+    //console.log(this.mydata);
+  }
+});
 
-//console.log(AudioVolume.audio)
+/* 
+<div class="insta-thumbnail" v-bind:style="{ 'background-image': 'url(' + instaData + ')' }"> */
 
-AudioAnalyzer.audio = AudioVolume.audio;
-AudioAnalyzer.init();
+const apps = new Vue({
+  el: "#app",
+  components: ["insta-card"],
+  data: {
+    instadata: []
+  },
+  created() {
+    axios.get("https://tuqulore.com/neurona/json/insta.php").then(response => {
+      this.instadata = response;
 
-/* $.ajax({
-	url: "img/logo.svg",
-	dataType: "xml",
-
-}).done((res)=> {
-	console.log(res)
-	$(".p-logo-main").append(res);
-}) */
+      console.log(response);
+    });
+    /* instajson.then(instadata => {
+			this.instadata = instadata;
+			console.log(instadata);
+			this.createInstaCard();
+		}); */
+  },
+  methods: {
+    createInstaCard: function() {
+      //console.log(this.instadata.data);
+    }
+  }
+});
