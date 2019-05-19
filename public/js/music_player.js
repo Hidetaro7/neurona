0f9dd29d73e0bde5779576eaeb0f0e5b18f5f289
Hidetaro7 <hidetaro7@gmail.com>
2019-05-19 16:30:59 +0900

    試聴スマホも対応

--- public/js/music_player.js
+++ public/js/music_player.js
@@ -1,8 +1,5 @@
 import pd from "./player-data.js";
 const playerData = pd.songs;
-let analyser;
-let frequencyData;
-let gainNode = 1; // audioContext.createCain
 
 const MusicPlayer = Vue.component("music-player", {
   props: {
@@ -15,9 +12,9 @@ const MusicPlayer = Vue.component("music-player", {
       currentPlayIndex: 0,
       audio: new Audio(),
       canvas: null,
-      isPlay: true,
-      isMute: true,
-      currentSongInfo: {}
+      currentSongInfo: {},
+      isPlay: false,
+      isMute: false
     };
   },
   computed: {},
@@ -27,13 +24,18 @@ const MusicPlayer = Vue.component("music-player", {
       this.currentSongInfo = {
         title: currentData.title,
         album: currentData.album,
-        src: currentData.src
+        src: currentData.src,
+        img: currentData.img
       };
-      //console.dir(this.audio);
       this.audio.src = playerData[this.currentPlayIndex].src;
-      this.audio.oncanplay = () => {
-        this.audio.play();
-      };
+      const playPromise = this.audio.play();
+      if (playPromise !== undefined) {
+        playPromise.catch(error => {
+          this.audio.muted = false;
+          this.audio.play();
+        });
+      }
+      //this.audio.oncanplay = () => {};
       this.audio.onended = () => {
         this.currentPlayIndex++;
         if (this.currentPlayIndex > playerData.length - 1) {
@@ -43,87 +45,32 @@ const MusicPlayer = Vue.component("music-player", {
       };
     },
     clickPlayBtn: function() {
-      if (this.isPlay) {
-        this.isPlay = false;
+      if (!this.audio.paused) {
         this.audio.pause();
       } else {
-        this.isPlay = true;
         this.audio.play();
       }
+      this.isPlay = !this.audio.paused;
     },
     clickMuteBtn: function() {
-      if (this.isMute) {
-        this.isMute = false;
-        gainNode.gain.value = 1;
-      } else {
-        this.isMute = true;
-        gainNode.gain.value = 0;
-      }
-    },
-    waveDisplay: function(audioElement, canvas) {
-      const canvas_ctx = canvas.getContext("2d");
-      window.AudioContext = window.AudioContext || window.webkitAudioContext;
-      const context = new AudioContext();
-      analyser = context.createAnalyser();
-      analyser.fftSize = 2048;
-      gainNode = context.createGain();
-      const source = context.createMediaElementSource(audioElement);
-      source.connect(analyser);
-      analyser.connect(gainNode);
-      gainNode.connect(context.destination);
-
-      frequencyData = new Uint8Array(analyser.frequencyBinCount);
-      //render
-      const render = () => {
-        requestAnimationFrame(render);
-        analyser.getByteFrequencyData(frequencyData);
-        canvas_ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        let ll = canvas.width;
-        for (let i = 0, l = ll; i < l; i++) {
-          let val = canvas.height - (canvas.height * frequencyData[i]) / 255;
-          canvas_ctx.beginPath();
-          canvas_ctx.moveTo(i * 2, val);
-          canvas_ctx.lineTo(i * 2, canvas.height);
-          let col = 360 * ((i * 2) / l);
-          if (this.isMute) {
-            canvas_ctx.strokeStyle = "rgba(50,50,50,.5)";
-          } else {
-            canvas_ctx.strokeStyle = `hsla(${col}, 50%, 50%, 0.4)`;
-          }
-          canvas_ctx.stroke();
-          canvas_ctx.closePath();
-        }
-      };
-      // canvas init
-      canvas.height = 100;
-      canvas.width = window.innerWidth;
-      window.onresize = () => {
-        canvas.width = window.innerWidth;
-      };
-      render();
+      this.audio.muted = !this.audio.muted;
+      this.isMute = this.audio.muted;
     }
   },
   mounted() {
-    this.canvas = this.$el.querySelector(".music-player-waver");
-    //document.body.ontouchend = () => {
-    this.waveDisplay(this.audio, this.canvas);
-
-    this.isMute = true;
-    gainNode.gain.value = 0;
     this.changeSong();
-    setTimeout(() => {}, 10);
-    //};
   },
   template: `
 	<div class="music-player">
-		<canvas class="music-player-waver"></canvas>
 		<div class="music-player-inner">
 			<div class="play-volume btn" :class="{'is-mute': isMute}" @click="clickMuteBtn"><i class="btn-mute-off fas fa-volume-mute"></i><i class="btn-mute-on fas fa-volume-up"></i><div class="tooltip">SOUND <span v-if="isMute">ON</span><span v-if="!isMute">OFF</span></div></div>
 			<div class="play-btn btn" :class="{'is-play': isPlay}" @click="clickPlayBtn"><i class="btn-play fas fa-play"></i><i class="btn-pause fas fa-pause"></i></div>
 			<div class="song-info">
-				<div class="song-title">「<span v-text="currentSongInfo.title"></span>」</div>
-				<div class="song-album" v-text="currentSongInfo.album"></div>
+				<img class="song-album-image" :src="currentSongInfo.img" :alt="currentSongInfo.album" />
+				<div class="song-info-main">
+					<div class="song-title">「<span v-text="currentSongInfo.title"></span>」</div>
+					<div class="song-album" v-text="currentSongInfo.album"></div>
+				</div>
 			</div>
 		</div>
 	</div>
--- public/js/player-data.js
+++ public/js/player-data.js
@@ -1,129 +1,190 @@
 const songData = {
   songs: [
     {
+      title: "ヒロインなんて大嫌い",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/01_heroine.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
+      title: "バタフライ",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/02_butterfly.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
+      title: "空と海と猫のワルツ",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/03_neko_waltz.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
+      title: "ピジョン夢物語",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/04_pigeon_yume.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
+      title: "ラスボスはもうひとりの君",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/05_lastboss.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
+      title: "ほとりの声",
+      album: "放出！アイデンティティ",
+      src: "audio/5th/06_hotorino_koe.m4a",
+      img: "./img/jacket-5th.jpg"
+    },
+    {
       title: "ハナビライフル",
       album: "玉虫色ライオット",
-      src: "audio/4th/01_hanabira.m4a"
+      src: "audio/4th/01_hanabira.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "夏空のドゥーワップ",
       album: "玉虫色ライオット",
-      src: "audio/4th/02_doo-wop.m4a"
+      src: "audio/4th/02_doo-wop.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "Cry",
       album: "玉虫色ライオット",
-      src: "audio/4th/03_cry.m4a"
+      src: "audio/4th/03_cry.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "サイレンス",
       album: "玉虫色ライオット",
-      src: "audio/4th/04_silence.m4a"
+      src: "audio/4th/04_silence.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "Wednesday Valentine",
       album: "玉虫色ライオット",
-      src: "audio/4th/05_wednesday_v.m4a"
+      src: "audio/4th/05_wednesday_v.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "サイのツノのように",
       album: "玉虫色ライオット",
-      src: "audio/4th/06_sai.m4a"
+      src: "audio/4th/06_sai.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "きみがきみのために",
       album: "玉虫色ライオット",
-      src: "audio/4th/07_kimikimi.m4a"
+      src: "audio/4th/07_kimikimi.m4a",
+      img: "./img/jacket-4th.jpg"
     },
     {
       title: "サイのツノのように",
       album: "サイのツノのように",
-      src: "audio/1st_single/sai.m4a"
+      src: "audio/1st_single/sai.m4a",
+      img: "./img/jacket_1st-single.jpg"
     },
     {
       title: "BOMB!",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/01_bomb.m4a"
+      src: "audio/3rd/01_bomb.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "Naturally（2018）",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/02_naturally.m4a"
+      src: "audio/3rd/02_naturally.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "So Sweet",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/03_so_sweet.m4a"
+      src: "audio/3rd/03_so_sweet.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "四足のヒーロー（2018）",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/04_4soku.m4a"
+      src: "audio/3rd/04_4soku.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "水蜜桃",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/05_suimitutou.m4a"
+      src: "audio/3rd/05_suimitutou.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "Hello Halo（未完成）",
       album: "空蝉ーUtsusemiー",
-      src: "audio/3rd/06_hello.m4a"
+      src: "audio/3rd/06_hello.m4a",
+      img: "./img/jacket-3rd.jpg"
     },
     {
       title: "Promise",
       album: "ナガスギタ春",
-      src: "audio/2nd/01_promise.m4a"
+      src: "audio/2nd/01_promise.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "Strange Day",
       album: "ナガスギタ春",
-      src: "audio/2nd/02_strangeday.m4a"
+      src: "audio/2nd/02_strangeday.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "DROPS",
       album: "ナガスギタ春",
-      src: "audio/2nd/03_drops.m4a"
+      src: "audio/2nd/03_drops.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "FRIEND",
       album: "ナガスギタ春",
-      src: "audio/2nd/04_friend.m4a"
+      src: "audio/2nd/04_friend.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "幾千一夜",
       album: "ナガスギタ春",
-      src: "audio/2nd/05_ikusen.m4a"
+      src: "audio/2nd/05_ikusen.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "チケット",
       album: "ナガスギタ春",
-      src: "audio/2nd/06_ticket.m4a"
+      src: "audio/2nd/06_ticket.m4a",
+      img: "./img/jacket-2nd_s.jpg"
     },
     {
       title: "四足のヒーロー",
       album: "突き放すよ、これが愛だ君への愛だ",
-      src: "audio/1st/01_4soku.m4a"
+      src: "audio/1st/01_4soku.m4a",
+      img: "./img/jacket-1st.jpg"
     },
     {
       title: "Naturally",
       album: "突き放すよ、これが愛だ君への愛だ",
-      src: "audio/1st/02_naturally.m4a"
+      src: "audio/1st/02_naturally.m4a",
+      img: "./img/jacket-1st.jpg"
     },
     {
       title: "ラバソニっ！",
       album: "突き放すよ、これが愛だ君への愛だ",
-      src: "audio/1st/03_loversonic.m4a"
+      src: "audio/1st/03_loversonic.m4a",
+      img: "./img/jacket-1st.jpg"
     },
     {
       title: "サイレンス",
       album: "突き放すよ、これが愛だ君への愛だ",
-      src: "audio/1st/04_silence.m4a"
+      src: "audio/1st/04_silence.m4a",
+      img: "./img/jacket-1st.jpg"
     },
     {
       title: "1000年のタイマー",
       album: "突き放すよ、これが愛だ君への愛だ",
-      src: "audio/1st/05_timer.m4a"
+      src: "audio/1st/05_timer.m4a",
+      img: "./img/jacket-1st.jpg"
     }
   ]
 };
